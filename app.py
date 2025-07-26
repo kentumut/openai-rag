43882d3e1c56be48e7920754a823ce4d4d8c0ae9
Kent Umut <kentumut@pm.me>
2025-07-26 09:04:54 +0300

    applied an hourly limit

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from openai_wrapper import get_answer, get_embedding
 from utils import build_prompt
 from qdrant_client import QdrantClient
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 COLLECTION_NAME = "rag_documents"
 client = QdrantClient(path="./qdrant_data")
 
+app.add_middleware(SlowAPIMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can restrict this to ["http://localhost:3000"] etc.
+    allow_origins=["kentumut.com"],  # You can restrict this to ["http://localhost:3000"] etc.
     allow_credentials=True,
     allow_methods=["*"],  # Or ["POST"] specifically
     allow_headers=["*"],
@@ -20,7 +29,8 @@ class QuestionRequest(BaseModel):
     question: str
 
 @app.post("/query")
-def query_rag(request: QuestionRequest):
+@limiter.limit("5/hour")
+def query_rag(request: QuestionRequest, req: Request):
     query_embedding = get_embedding(request.question)
     search_result = client.search(
         collection_name=COLLECTION_NAME,
